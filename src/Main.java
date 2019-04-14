8be2b85879a4b32ad1460981f7d4845e0cb6c8a5
marianbalan <marianbalan2@yahoo.com>
2019-04-14 10:41:22 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("numele primului jucator");
+        String player1Name = scanner.nextLine();
+        System.out.println("numele ")
+
+    }
+}
---
+++ src/MyTicTacToe.java
@@ -0,0 +1,60 @@
+public class MyTicTacToe {
+
+     public static final char SYMBOL_X = `X`;
+     public static final char SYMBOL_0 = `0`;
+
+    public static final int GAME_SIZE = 3;
+
+
+    char [] [] game = new char [GAME_SIZE][GAME_SIZE];
+
+    Player player1;
+    Player player2;
+
+    public MyTicTacToe(Player player1,Player player2){
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public void showGame(){
+        for (int i = 0; i < GAME_SIZE; i++){
+            for (int j = 0; j< GAME_SIZE; i++)
+        }
+        System.out.print(game [i][j]);
+
+    }
+
+}
+      public void initBoard(){
+    for (int i = 0;i < GAME_SIZE; i++){
+        for ( int j = 0;j < GAME_SIZE;i=j++){
+            game[i][j]= `.`;
+        }
+    }
+
+     public boolean isWin(){
+        //testez linii
+        //testez coloane
+
+          }
+
+     public void playGame() {
+        initBoard();
+        System.out.println(" incepe jocul ");
+        showGame();
+
+
+
+        Player currentPlayer = player1;
+        int nrMoves = 0;
+        boolean isWin = false ;
+
+        while(isWin == false && nrMoves < 9){
+
+            //citesc mutare
+            //validez mutare
+            //efectuez mutare
+            //numar mutare
+        }
+     }
+}
